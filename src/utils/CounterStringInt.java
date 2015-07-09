106ee9dfbfbee85cb1afeae9473f5f4290bfb5e0
jmoura <jmoura@jmoura-w7m.int.pason.com>
2015-07-09 13:48:11 -0600

    -Creating utils

---
+++ src/utils/CounterStringInt.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import java.io.Serializable;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+/**
+ * servers to concatenate strings
+ *
+ * @author jmoura
+ */
+public final class CounterStringInt implements Serializable {
+
+    private final SortedMap<String, Integer> counter = new TreeMap<String, Integer>();
+
+    public SortedMap<String, Integer> getCounter() {
+        return counter;
+    }
+
+    public Set<String> getKeys() {
+        return counter.keySet();
+    }
+
+    public int getTotal() {
+        int ret = 0;
+        for (Integer qtd : counter.values()) {
+            ret += qtd;
+        }
+        return ret;
+    }
+
+    public int getValue(String key) {
+        return counter.get(key);
+    }
+
+    public void add(String key, int value) {
+        if (counter.containsKey(key)) {
+            counter.put(key, counter.get(key) + value);
+        } else {
+            counter.put(key, value);
+        }
+    }
+}
