8c6b3b3ac8cb3de91e5c9084631b3abc70823ce3
jmoura <jmoura@jmoura-w7m.int.pason.com>
2016-09-19 19:34:44 -0600

    -Deploy PbmCommons to PbmCounselor

--- src/persistence/BundleManager.java
+++ src/persistence/BundleManager.java
@@ -22,18 +22,17 @@ public class BundleManager implements Serializable {
     }
 
     public String getString(String label) {
-        String ret = "N/A (Missing Translation)";
         try {
-            ret = ResourceBundle.getBundle("labels").getString(label);
+            return ResourceBundle.getBundle("labels").getString(label);
         } catch (MissingResourceException e) {
-            try {
-                ret = ResourceBundle.getBundle("mensagens").getString(label);
-            } catch (MissingResourceException ex) {
-                //YYY: to de/activate the fatal stop when a string is not found
-//                throw new UnsupportedOperationException("Missing string: " + label);
-                log.fatal("Missing string: " + label);
-            }
         }
-        return ret;
+        try {
+            return ResourceBundle.getBundle("mensagens").getString(label);
+        } catch (MissingResourceException ex) {
+        }
+        //YYY: to de/activate the fatal stop when a string is not found
+//                throw new UnsupportedOperationException("Missing string: " + label);
+        log.fatal("Missing string: " + label);
+        return "N/A (Missing Translation)";
     }
 }
