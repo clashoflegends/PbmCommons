f92b5e06de7d0ad9da691ca0a8443982850b3b11
clashGM01 <clashoflegends.thegame@gmail.com>
2019-04-18 11:04:27 -0600

    Remove city resources from WDO games Fix BattleSim Crash when new armies are being added

--- src/business/combat/ArmySim.java
+++ src/business/combat/ArmySim.java
@@ -37,10 +37,11 @@ public class ArmySim extends BaseModel implements IExercito {
     private SortedMap<String, Pelotao> platoons = new TreeMap();
     private List<TipoTropa> troops = new ArrayList<TipoTropa>();
 
-    public ArmySim(String name, Terreno terrain) {
+    public ArmySim(String name, Terreno terrain, Nacao nation) {
         this.setNome(name);
         this.comandanteNome = name;
         this.terreno = terrain;
+        this.nacao = nation;
         //FIXME: needs to receive Local for the Battle to be resolved. Deal with this later.
     }
 
