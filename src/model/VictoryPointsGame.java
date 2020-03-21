17ec6fc0a768d620869c50fb8966d768b586f5e6
clashGM01 <clashoflegends.thegame@gmail.com>
2020-03-21 16:52:58 -0600

    Adding victory points for all turns, to create an evolution graph in Counselor

---
+++ src/model/VictoryPointsGame.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+/**
+ * Prepping and structuring data for JavaFX Graphs in Counselor. 
+ * @author gurgel
+ */
+public class VictoryPointsGame implements Serializable {
+
+    private final SortedMap<Nacao, SortedMap<Integer, Integer>> nationsPoints = new TreeMap(); //<Nation, <Turn, Points>
+    private final Set<Integer> turnList = new TreeSet();
+
+    public void addPoints(Nacao nation, int turn, int points) {
+        //check if nation exists
+        if (nationsPoints.containsKey(nation)) {
+            nationsPoints.get(nation).put(turn, points);
+        } else {
+            //add nation and set
+            final SortedMap newMap = new TreeMap();
+            newMap.put(turn, points);
+            nationsPoints.put(nation, newMap);
+        }
+        //update list of turns for easy keys extraction
+        getTurnList().add(turn);
+    }
+
+    public Set<Integer> getTurnList() {
+        return turnList;
+    }
+
+    public Set<Nacao> getNationsList() {
+        return nationsPoints.keySet();
+    }
+}
