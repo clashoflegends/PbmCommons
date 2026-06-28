1b46211845759a5004d67768b29915e52881ef9e
Clash GM <clashoflegends>
2026-06-28 16:08:11 -0600

    Portraits: GitHub-release pipeline interface + auto-download labels
    DownloadPortraitsService is now URL-based (downloadPortraitsZip(url,folder)+getSize(url));
    dropped the GoDaddy manifest. Add auto-download portrait-update labels.

--- src/business/DownloadPortraitsService.java
+++ src/business/DownloadPortraitsService.java
@@ -7,9 +7,7 @@ package business;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.net.ConnectException;
-import java.util.Properties;
 
 /**
  *
@@ -19,10 +17,10 @@ public interface DownloadPortraitsService {
     
     
     public void checkNetworkConnection() throws ConnectException;
-    
-    public Properties getServerPropertiesFile() throws IOException;
-    
-    public File downloadPortraisFile(String portraitsFileName, String portraitsFolder) throws FileNotFoundException;
-    
-    public int getSize(String portraitsFileName) throws FileNotFoundException;
+
+    /** Download the portraits zip from {@code url} into {@code destFolder} (as portraits.zip). */
+    public File downloadPortraitsZip(String url, String destFolder) throws FileNotFoundException;
+
+    /** Size in bytes of the zip at {@code url} (HEAD, follows redirects); 0 if unknown. */
+    public int getSize(String url) throws FileNotFoundException;
 }
